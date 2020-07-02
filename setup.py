5fb799d7df869be9131729b24865221b5db1d232
Andy Dang🤖 <andy@whylabs.ai>
2020-07-02 15:44:28 -0700

    Use bump2version to manage versions

--- setup.py
+++ setup.py
@@ -20,12 +20,34 @@
 
 import os
 import sys
-import sysconfig
 import platform
 import subprocess
 
+import codecs
+import os.path
+
 from setuptools import setup, find_packages, Extension
 from setuptools.command.build_ext import build_ext
+
+
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
+
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith('__version__'):
+            delimiter = '"' if '"' in line else "'"
+            v = line.split(delimiter)[1]
+            print(f'WhyLogs version: {v}')
+            return v
+    else:
+        raise RuntimeError("Unable to find version string.")
+
+
+version = get_version("python/src/_version.py")
 
 class CMakeExtension(Extension):
     def __init__(self, name, sourcedir=''):
@@ -84,7 +106,7 @@ class CMakeBuild(build_ext):
 setup(
     name='datasketches',
     #use_scm_version=True,
-    version='2.0.0-incubating-SNAPSHOT',
+    version=version,
     author='Datasketches Developers',
     author_email='dev@datasketches.apache.org',
     description='A wrapper for the C++ Datasketches library',
