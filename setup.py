2ba577a68197c86bfcbf788a8d95f6ddd8db8f8d
Andy Dang🤖 <andy@whylabs.ai>
2020-07-27 10:29:11 -0700

    Enable Pybind to detect different Python version from the default Path

--- setup.py
+++ setup.py
@@ -77,6 +77,8 @@ class CMakeBuild(build_ext):
             os.path.dirname(self.get_ext_fullpath(ext.name)))
         cmake_args =  ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir]
         cmake_args += ['-DWITH_PYTHON=True']
+        # Require to enable Pybind11 to pick up the correct Python paths
+        cmake_args += ['-DPYTHON_EXECUTABLE=' + sys.executable]
         cfg = 'Debug' if self.debug else 'Release'
         build_args = ['--config', cfg]
 
